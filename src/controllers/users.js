b9ebe1f0a503e795277e81c4432000597a3bbc69
unknown <gregory.barros@hotmail.com>
2020-02-15 12:53:38 -0300

    Adicionado get, put, delete a rota de usuarios

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -4,7 +4,7 @@ const Usuario = require('../models/Usuario')
 
 const router = express.Router()
 
-router.use(authMiddleware)
+//router.use(authMiddleware)
 
 router.get('/', async (req, res) => {
 
@@ -17,6 +17,66 @@ router.get('/', async (req, res) => {
 
     }  
       
+})
+
+router.get('/:id', async (req, res) => {
+
+    const { id } = req.params
+
+    try {
+        const FindUser = await Usuario.findOne({ _id: req.params.id })
+
+        return res.send(FindUser)
+    } catch (err) {
+
+        res.status(400).send('Usuario nao encontrado'+err)
+
+    }
+
+})
+
+router.put('/edit', async (req, res) => {
+
+    try {
+        let EditUser = await Usuario.findOne({ _id: req.body._id }).updateOne({
+
+            nome: req.body.nome,
+            sobrenome: req.body.sobrenome,
+            email: req.body.email,
+            cidade: req.bodycidade,
+            cep: req.body.cep,
+            endereco: req.body.endereco,
+            estado:req.body.estado,
+            senha: req.body.senha,
+            dataNas: req.body.dataNas,
+            dataAdm: req.body.dataAdm,
+            sexo: req.body.sexo
+        })
+        console.log(EditUser)
+        return res.send(EditUser)
+    } catch (err) {
+
+        res.status(400).send('Erro ao editar usuario!'+err)
+        
+    }
+
+})
+
+router.delete('/delete/:id', async (req, res) => {
+
+    const { id } = req.params
+
+
+    try {
+        await Usuario.findOneAndDelete({ _id: req.params.id })
+    
+        return res.send('Usuario deletado com sucesso')
+    } catch (err) {
+
+        res.status(400).send('Erro ao deletar usuario!'+err)
+        
+    }
+
 })
 
 module.exports = app => app.use('/users', router)
